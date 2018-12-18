61b2b03c5ea8b847cc353a5671e214e030fb7774
xdc0209 <xdc0209@qq.com>
2018-12-19 00:29:28 +0800

    优化终端标题

--- 01_configure_linux/02_configure_command_prompt/command_prompt.bashrc.sh
+++ 01_configure_linux/02_configure_command_prompt/command_prompt.bashrc.sh
@@ -2,4 +2,7 @@
 # 优化命令提示符##
 [ $UID -eq 0 ] && export PS1='\[\e[01;31m\]\u@\h\[\e[00m\]:\[\e[01;35m\]\w\[\e[00m\]\$ '
 [ $UID -ne 0 ] && export PS1='\[\e[01;32m\]\u@\h\[\e[00m\]:\[\e[01;35m\]\w\[\e[00m\]\$ '
+
+# 优化终端标题##
+SSH_SERVER=$(echo $SSH_CONNECTION | awk '{print $3}') && [ -n "$SSH_SERVER" ] && echo -en "\e]0;$SSH_SERVER\a"
 # ----------------------------- kiwi command prompt end ---------------------------------
