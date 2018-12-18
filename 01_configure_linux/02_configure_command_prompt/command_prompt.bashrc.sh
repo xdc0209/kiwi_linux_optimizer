e7a1b8acca37ed3dba0df6298d3e3fcce339195c
xdc0209 <xdc0209@qq.com>
2018-12-19 00:51:37 +0800

    优化终端标题

--- 01_configure_linux/02_configure_command_prompt/command_prompt.bashrc.sh
+++ 01_configure_linux/02_configure_command_prompt/command_prompt.bashrc.sh
@@ -3,6 +3,6 @@
 [ $UID -eq 0 ] && export PS1='\[\e[01;31m\]\u@\h\[\e[00m\]:\[\e[01;35m\]\w\[\e[00m\]\$ '
 [ $UID -ne 0 ] && export PS1='\[\e[01;32m\]\u@\h\[\e[00m\]:\[\e[01;35m\]\w\[\e[00m\]\$ '
 
-# 优化终端标题##
+# 优化终端标题，再配合Xshell，达到在标签上显示当前机器IP的效果，Xshell配置：右键标签--显示终端标题--勾选上##
 SSH_SERVER=$(echo $SSH_CONNECTION | awk '{print $3}') && [ -n "$SSH_SERVER" ] && echo -en "\e]0;$SSH_SERVER\a"
 # ----------------------------- kiwi command prompt end ---------------------------------
